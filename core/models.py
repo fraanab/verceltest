6e503b225da5908102ca6b9706e8387580fc40c9
sentey <91223447+fraanab@users.noreply.github.com>
2023-04-21 17:10:33 -0300

    db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Notes(models.Model):
+	username = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+	text = models.TextField(max_length = 255)
+	date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'User: {self.username}, note: {self.text}'
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Notes
+
 
 # Create your views here.
 def main(request):
-	return render(request, 'main.html')
+	allnotes = Notes.objects.all()
+	return render(request, 'main.html', {'allnotes':allnotes})
